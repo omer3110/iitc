39e61c8d11d53d64a488c843b8486c2380485bd4
omer3110 <omer.sidi10@gmail.com>
2024-05-05 20:31:42 +0300

    Exc7

---
+++ exc7/sortedArrayQ4intest/main.js
@@ -0,0 +1,21 @@
+//כתוב פונקציה שמקבלת מערך של מספרים ומחזירה מערך ממוין מהקטן לגדול.
+// אסור לשנות את המערך המקורי. אסור להשתמש בפונקצית sort של מערך. מה סבוכיות הריצה ?
+
+function sortArray(array) {
+    let tempdArr = array.slice(); // Make a copy of the input array
+    let sortedArray = [];
+    while (tempdArr.length > 0) {
+        let minIndex = 0;
+        for (let i = 1; i < tempdArr.length; i++) {
+            if (tempdArr[i] < tempdArr[minIndex]) {
+                minIndex = i;
+            }
+        }
+        sortedArray.push(tempdArr[minIndex]);
+        tempdArr.splice(minIndex, 1);
+    }
+    return sortedArray;
+}
+
+let numArray = [2, 4, 1, 6, 5, 87, 3, 2];
+console.log(sortArray(numArray));
---
+++ local-storage/todos-app.html/main.js
@@ -0,0 +1,67 @@
+
+// localStorage.removeItem("checkList");
+
+let toDo = document.querySelector("#my-to-do");
+let checkList = document.querySelector(".checkList");
+let toDoArray = [];
+console.log(toDoArray);
+localStorageLoad();
+
+// Function to load data from local storage
+function localStorageLoad() {
+    let currentStorage = localStorage.getItem("checkList");
+    if (currentStorage) {
+        toDoArray = JSON.parse(currentStorage);
+        toDoArray.forEach(addToDoItem);
+    }
+}
+// Function to add a to-do item to the list
+function addToCheckList() {
+    let newToDo = toDo.value.trim();
+    if (checkUniqueToDo(newToDo)) {
+        addToDoItem(newToDo);
+        toDoArray.push(newToDo);
+        localStorage.setItem("checkList", JSON.stringify(toDoArray));
+        toDo.value = ''; // Clear input field after adding
+    }
+}
+// Function to check for duplicate and empty to-do items
+function checkUniqueToDo(checkRow) {
+    if (toDoArray.includes(checkRow) || checkRow === '') {
+        alert("Cannot insert duplicates or empty to-dos");
+        return false;
+    }
+    return true;
+}
+// Function to add a to-do item to the list
+function addToDoItem(item) {
+    let li = document.createElement('li');
+    li.classList.add('row');
+    li.textContent = item;
+    checkList.appendChild(li);
+    li.onclick = function () {
+        // li.remove();
+        // let index = toDoArray.indexOf(item);
+        // toDoArray.splice(index, 1);
+        // localStorage.setItem("checkList", JSON.stringify(toDoArray));
+        if (li.style.color == "black") {
+            li.style.color = "green";
+        }
+        else {
+            li.style.color = "black"
+        }
+    };
+}
+// Function to sort the list
+function sortList() {
+    toDoArray.sort();
+    checkList.innerHTML = ''; // Clear the list
+    toDoArray.forEach(addToDoItem);
+    localStorage.setItem("checkList", JSON.stringify(toDoArray));
+}
+
+function clearCheckList() {
+    toDoArray = [];
+    localStorage.setItem("checkList", JSON.stringify(toDoArray));
+    checkList.innerHTML = '';
+}
